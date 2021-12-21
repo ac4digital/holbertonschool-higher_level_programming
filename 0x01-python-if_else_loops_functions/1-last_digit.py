4ba2929ce481c9ac30827188b1b4a3d26857310c
Andrés Cataño <3866@holbertonschool.com>
2021-12-21 18:18:04 -0500

    First commit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+msg = "Last digit of"
+module = number % 10
+
+if number < 0:
+    module = number % -10
+else:
+    module = number % 10
+
+print("Last digit of", number, "is", module, end=" ")
+
+if module > 5:
+    print("and is greater than 5")
+elif module == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
+    
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+
+    for i in str:
+        letter = ord(i)
+
+        if letter > 96 and letter < 123
+            letter = letter - 32
+        print("{:c}".format(i), end="")
+
+    print()
