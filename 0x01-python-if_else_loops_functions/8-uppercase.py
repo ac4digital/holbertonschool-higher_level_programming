9f6243c482791f37c606f533910f723d66975eed
Andrés Cataño <3866@holbertonschool.com>
2021-12-21 19:58:17 -0500

    Fix Pycodestyle

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
 def uppercase(str):
-
     for i in str:
         letter = ord(i)
 
-        if letter > 96 and letter < 123
+        if letter > 96 and letter < 123:
             letter = letter - 32
         print("{:c}".format(i), end="")
 
