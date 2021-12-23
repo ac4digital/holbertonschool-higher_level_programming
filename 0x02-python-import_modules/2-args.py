1ae208d6922b3846edda5740f03cc73722b627c5
Andrés Cataño <3866@holbertonschool.com>
2021-12-23 17:46:56 -0500

    Fix 1 Argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     length = len(argv)
-    print("{:d} {:s}{:c}".format(length - 1, "argument" if length < 2 else "arguments",
+    print("{:d} {:s}{:s}".format(length - 1, "argument" if length <= 2 else "arguments",
     "." if length == 1 else ":"))
     for i, string in enumerate(argv):
         if i > 0:
