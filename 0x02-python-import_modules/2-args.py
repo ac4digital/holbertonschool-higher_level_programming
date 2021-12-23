e9584405b5271aef4a25bc757c4f52be5e2189e3
Andrés Cataño <3866@holbertonschool.com>
2021-12-23 18:01:57 -0500

    Fix pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,9 @@
 if __name__ == "__main__":
     from sys import argv
     length = len(argv) - 1
-    print("{:d} {:s}{:s}".format(length, "argument" if length == 1 else "arguments",
-    "." if length == 0 else ":"))
+    print("{:d} {:s}{:s}".format(length,
+                                 "argument" if length == 1 else "arguments",
+                                 "." if length == 0 else ":"))
     for i, string in enumerate(argv):
         if i > 0:
             print("{:d}: {:s}".format(i, string))
