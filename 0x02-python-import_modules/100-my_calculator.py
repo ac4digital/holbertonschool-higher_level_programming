c966629d5afe91d8d7c1757c8c1b29ce58b25fdb
Andrés Cataño <3866@holbertonschool.com>
2021-12-23 19:20:45 -0500

    First Commit

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    length = len(argv)
+    if length != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b.")
+        quit(1)
+    functions = [add, sub, mul, div]
+    operators = ["+", "-", "*", "/"]
+    a = int(argv[1])
+    b = int(argv[3])
+    for i, s in enumerate(operators):
+        if argv[2] == s:
+            print("{:d} {:s} {:d} = {:d}".format(a, s, b, functions[i](a, b)))
+            break;
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            quit(1)
