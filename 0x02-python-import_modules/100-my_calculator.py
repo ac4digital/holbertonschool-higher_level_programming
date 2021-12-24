f8ee1693e78d148bb8c7eea94684bcd699b66885
Andrés Cataño <3866@holbertonschool.com>
2021-12-23 19:21:58 -0500

    Fix pycodestyle ;

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     for i, s in enumerate(operators):
         if argv[2] == s:
             print("{:d} {:s} {:d} = {:d}".format(a, s, b, functions[i](a, b)))
-            break;
+            break
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             quit(1)
