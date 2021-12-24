d035242370a1c1a414f85ba51843656393e4f03e
Andrés Cataño <3866@holbertonschool.com>
2021-12-23 19:35:08 -0500

    Fix Format {}

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     b = int(argv[3])
     for i, s in enumerate(operators):
         if argv[2] == s:
-            print("{:d} {:s} {:d} = {:d}".format(a, s, b, functions[i](a, b)))
+            print("{} {} {} = {}".format(a, s, b, functions[i](a, b)))
             break
         else:
             print("Unknown operator. Available operators: +, -, * and /")
