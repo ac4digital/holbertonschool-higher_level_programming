48143f118059deedc20df023d577c2f94085455c
Andrés Cataño <3866@holbertonschool.com>
2022-01-26 14:03:51 -0500

    First Commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,8 +2,4 @@
 class LockedClass:
     """A locked class that prevents the user from dinamically creating new instance attributes,
     except if the new instance attribute is called 'first_name'"""
-    def __setattr__(self, name, value):
-        if name == "first_name":
-            self.__dict__[name] = value
-        else:
-            raise AttributeError("'LockedClass' object has no attribute '" + name + "'")
+    __slots__ = ['first_name']
