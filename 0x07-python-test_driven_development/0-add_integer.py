084f9f89e9f1cb8120a1966e35bb7198076ee91c
Andrés Cataño <3866@holbertonschool.com>
2022-01-27 11:02:48 -0500

    First Commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-'''
+"""
 Function that returns the add of two integer numbers
-'''
+"""
+
 
 def add_integer(a, b=98):
     '''Function that returns the add of two integer numbers'''
-    if (type (a) is float):
+
+    if (type(a) is float):
         a = int(a)
-    if (type (b) is float):
+    if (type(b) is float):
         a = int(b)
-    if(type(a) is not int):
+    if (type(a) is not int):
         raise TypeError("a must be an integer")
-    if(type(b) is not int):
+    if (type(b) is not int):
         raise TypeError("b must be an integer")
 
     return (a + b)
