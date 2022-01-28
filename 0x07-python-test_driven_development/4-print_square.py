f6992bfdcd7b8bee6b1d141ab9610c0c5a677abd
Andrés Cataño <3866@holbertonschool.com>
2022-01-28 08:42:46 -0500

    First Commit

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Function that prints a square with the character "#"
+'''
+def print_square(size):
+    '''
+    Function that prints a square with the character "#"
+    '''
+    if (type(size) is not int):
+        raise TypeError("size must be an integer")
+    if (size < 0):
+        raise ValueError("size must be >= 0")
+    else:
+        for i in range(0, size):
+            for j in range(0, size):
+                print("#", end="")
+            print()
