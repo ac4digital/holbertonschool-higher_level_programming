f177127a9596db7e0a2172eb67b14fe1ebad9200
Andrés Cataño <3866@holbertonschool.com>
2022-01-28 14:01:15 -0500

    First Commit

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,15 +7,19 @@ def text_indentation(text):
     
     jumps = ['.', '?', ':']
     new_string = ""
-    flags = 0
+    flags = 1
+
     for s in text:
+        if flags == 1:
+            if s == ' ':
+                continue
+            else:
+                flags = 0
         if flags == 0:
-            new_string += s
             if s in jumps:
+                new_string += s
                 new_string += "\n\n"
                 flags = 1
             else:
-                flags = 0
-        elif flags == 1 and s == " ":
-            flags = 0
-    print(new_string, end="")
+                new_string += s
+    print(new_string, end='')
