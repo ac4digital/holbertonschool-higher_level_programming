76605d9394fed33e9c07d3c77c7372781d3b7bc7
Andrés Cataño <3866@holbertonschool.com>
2022-01-28 09:22:00 -0500

    First Commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,7 +10,7 @@ def add_integer(a, b=98):
     if (type(a) is float):
         a = int(a)
     if (type(b) is float):
-        a = int(b)
+        b = int(b)
     if (type(a) is not int):
         raise TypeError("a must be an integer")
     if (type(b) is not int):
