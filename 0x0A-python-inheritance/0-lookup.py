e4cad864a045d5a416153cbd429503807401f084
Andrés Cataño <3866@holbertonschool.com>
2022-02-01 18:32:45 -0500

    Fixing Pep8 Style

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 Function that returns the list of
 available attributes and methods of an object
 """
@@ -7,7 +7,7 @@ available attributes and methods of an object
 
 def lookup(obj):
     """
-    Function that returns the list of available 
+    Function that returns the list of available
     attributes and methods of an object
     """
     return dir(obj)
