083570e7bcef80f0f0269f8153b02a94eaff7517
Andrés Cataño <3866@holbertonschool.com>
2022-02-01 22:10:21 -0500

    Fix Pep8 Style

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,8 +6,8 @@ Contains the class BaseGeometry
 
 def add_attribute(obj, name, value):
     """Adds a new attribute if is possible"""
-    types = (int, float, complex, set, dict, tuple, 
-    list, bool, frozenset, type, object, str)
+    types = (int, float, complex, set, dict, tuple,
+             list, bool, frozenset, type, object, str)
     for _type in types:
         if type(obj) is _type:
             raise TypeError("can't add new attribute")
