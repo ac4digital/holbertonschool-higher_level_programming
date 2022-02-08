4f317f25852fdf1a187b092fad95acfab3b941c1
Andrés Cataño <3866@holbertonschool.com>
2022-02-07 21:37:31 -0500

    First Commit

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -8,7 +8,7 @@ def pascal_triangle(n):
     """returns a list of lists of integers
     representing the Pascal's triangle of n"""
 
-    if n < 0:
+    if n <= 0:
         return[]
 
     triangle = [[1]]
