59125650fc74d116f7d37d6ac5bb64d9b06d66df
Andres Cataño <3866@holbertonschool.com>
2022-02-12 18:44:04 -0500

    JSON Class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Define a Class Base"""
+import json
 
 
 class Base:
@@ -16,3 +17,28 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        return the JSON string representation
+        of list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Method that writes the JSON string
+        representation of list_objs to a file
+        """
+        filename = cls.__name__ + '.json'
+        list = []
+        if list_objs is not None:
+            for i in list_objs:
+                list.append(i.to_dictionary())
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(list))
