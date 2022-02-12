0273a7010ba7a84003aef772f65ea20e6ea2d317
Andres Cataño <3866@holbertonschool.com>
2022-02-12 17:58:41 -0500

    Base Class Task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define a Class Base"""
+
+class Base:
+    """
+    Class Bass will be the "Base"
+    of all other classes
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
