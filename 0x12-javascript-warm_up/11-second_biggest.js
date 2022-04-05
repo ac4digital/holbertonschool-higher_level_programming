f1f1ebc718d861ba57057a32cf24204203448fe4
Andrés Cataño <3866@holbertonschool.com>
2022-04-05 09:32:13 -0500

    First Commit

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const nextMax = process.argv;
+const args = [];
+if (nextMax.length <= 3) {
+  console.log(0);
+} else {
+  for(let i = 2; i < nextMax.length; i++) {
+    args.push(parseInt(nextMax[i]));
+    args.sort((a, b) => a - b);
+  }
+  const index = args.length;
+  const max = args[index - 2];
+  console.log(max);
+}
+exports.nextMax = nextMax;
