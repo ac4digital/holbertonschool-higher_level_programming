e970bcad01484297224a3d2b346f3311f3218a0c
Andrés Cataño <3866@holbertonschool.com>
2022-04-04 21:13:43 -0500

    First Commit

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let arg in args) {
   console.log(args[arg]);
 }
