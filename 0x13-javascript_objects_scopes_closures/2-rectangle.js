9750df6489cfdbcd4bf188c6730a568d4d21858a
Andrés Cataño <3866@holbertonschool.com>
2022-04-07 21:41:12 -0500

    First Commit

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,9 +4,7 @@ class Rectangle {
     if (w > 0 && h > 0 && w) {
       this.width = w;
       this.height = h;
-    } else {
-      return undefined;
-      }
     }
+  }
 }
 module.exports = Rectangle;
