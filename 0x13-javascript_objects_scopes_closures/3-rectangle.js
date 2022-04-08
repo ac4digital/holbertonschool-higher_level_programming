5af6b2f7ade6b706d66dada4aad70941d74a9b4c
Andrés Cataño <3866@holbertonschool.com>
2022-04-07 21:23:34 -0500

    First Commit

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return this;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return undefined;
     }
-    this.width = w;
-    this.height = h;
   }
   print () {
     for (let i = 0; i < this.height; i++) {
