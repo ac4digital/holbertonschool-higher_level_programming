9e639e14458c1482f4e158cb3fc26c45c56331e4
Andrés Cataño <3866@holbertonschool.com>
2022-04-08 10:04:24 -0500

    First Commit

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle.js');
 
-class Square extends Square1 {
+class Square extends Rectangle {
   charPrint (c) {
     if (c === undefined) {
       this.print();
