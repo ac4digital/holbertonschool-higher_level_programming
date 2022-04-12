5b8f7f810cd72c0b8d095f8d53afff7f4151586f
Andrés Cataño <3866@holbertonschool.com>
2022-04-11 23:15:45 -0500

    First Commit

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const url = 'https://swapi-api.hbtn.io/api/films/'
+const movie = process.argv[2];
+const request = require('request');
+
+request(url + movie, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else if (res.statusCode === 200) {
+    const film = JSON.parse(body);
+    for (let i in film.characters) {
+        request(film.characters[i], function (err, res, body) {
+            if (err) {
+                console.log(err);
+            } else if (res.statusCode === 200) {
+                const character = JSON.parse(body);
+                console.log(character.name);
+            }
+        })
+        //console.log(film.characters[i]);
+    }
+  } else {
+    console.log('Error: ' + res.statusCode);
+  }
+}
+);
