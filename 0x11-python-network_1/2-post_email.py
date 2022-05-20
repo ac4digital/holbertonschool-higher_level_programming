c478e0635a525356b2952488c62dca9c45e53cf8
Andrés Cataño <3866@holbertonschool.com>
2022-05-19 21:30:09 -0500

    First Commit

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Script tha takes in a URL and an email, sends a POST request to the
-passed URL with the email as a parameter, and displays the body of the response.
+Script tha takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response.
 """
 if __name__ == "__main__":
     import urllib.parse as parse
